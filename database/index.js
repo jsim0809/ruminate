00a7419bf9beb92f49dd804256c613f32bee6951
Jeremy Sim <jsim0809@gmail.com>
2020-04-08 13:33:08 -0700

    Clears up directories for rewriting API path

--- database/index.js
+++ database/index.js
@@ -15,22 +15,3 @@ module.exports.getAllReviews = (restaurantId, callback) => {
       callback(error);
     });
 };
-
-module.exports.getSummary = (restaurantId, callback) => {
-  // get restaurant summary info from restaurant table
-  const client = createClient();
-  const sql = squel.select()
-    .from('restaurants')
-    .field('restaurants.location')
-    .field('restaurants.noise')
-    .field('restaurants.recommendpercent', 'recommendPercent')
-    .field('restaurants.valuerating', 'valueRating')
-    .field('restaurants.averageoverall', 'averageOverall')
-    .field('restaurants.averagefood', 'averageFood')
-    .field('restaurants.averageambience', 'averageAmbience')
-    .field('restaurants.averageservice', 'averageService')
-    .where(`id = ${restaurantId}`)
-    .toString();
-
-  makeQuery(client, sql, callback);
-};
